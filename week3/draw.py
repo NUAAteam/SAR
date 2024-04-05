dc737e42c273d508a8600aae4165c3b0a1365351
Ebotian_win <yiboxiaotian@nuaa.edu.cn>
2024-04-05 15:43:16 +0800

    实现了面积

--- week3/draw.py
+++ week3/draw.py
@@ -26,7 +26,7 @@ class Point:
         return 1-int(random.random()+1-self.pab())
     # 计算点的面积（正态分布N(dist, sigma))
     def area(self):
-        return 1/(2*math.pi*self.sigma)*math.exp(-self.dist()**2/(2*self.sigma))
+        return random.normalvariate(self.dist(), self.sigma)
 
 # 初始值
 # 间隔
@@ -41,29 +41,6 @@ jc=50
 k=500
 sigma=1
 
-def draw_many_points(dm, dn, ic, jc, k, sigma):
-    x = []
-    y = []
-    for i in np.arange(0, 100, dm):
-        for j in np.arange(0, 100, dn):
-            p = Point(i, j, ic, jc, k, sigma)
-            if p.status() == 1:
-                x.append(j)  # Note that j is the x-coordinate
-                y.append(i)  # Note that i is the y-coordinate
-    fig = go.Figure(data=go.Scattergl(x=x, y=y, mode='markers',
-                                      marker=dict(color='blue', size=2)))
-    #fig.show()
-    fig.update_layout(
-        autosize=False,
-        width=500,
-        height=500,
-        xaxis=dict(
-            scaleanchor="y",
-            scaleratio=1,
-        ),
-    )
-    return fig
-
 def draw_many_splinter(dm, dn, ic, jc, k, sigma):
     x = []
     y = []
@@ -73,22 +50,39 @@ def draw_many_splinter(dm, dn, ic, jc, k, sigma):
 #在碎片的每个边上随机选取 1 个点，得到四个随机点
 #采用计算机图形学中的数值微分直线生成法连接四个点，得到一个碎片
 #生成所有碎片
+
     for i in np.arange(0, 100, dm):
         for j in np.arange(0, 100, dn):
             p = Point(i, j, ic, jc, k, sigma)
             if p.status() == 1:
-                x1 = i-dm/2
-                y1 = random.uniform(j-dn/2,j+dn/2)
-                x2 = random.uniform(i-dm/2,i+dm/2)
-                y2 = j-dn/2
-                x3 = i+dm/2
-                y3 = random.uniform(j-dn/2,j+dn/2)
-                x4 = random.uniform(i-dm/2,i+dm/2)
-                y4 = j+dn/2
+               # x1 = i-dm/2
+               # y1 = random.uniform(j-dn/2,j+dn/2)
+               # x2 = random.uniform(i-dm/2,i+dm/2)
+               # y2 = j-dn/2
+               # x3 = i+dm/2
+               # y3 = random.uniform(j-dn/2,j+dn/2)
+               # x4 = random.uniform(i-dm/2,i+dm/2)
+               # y4 = j+dn/2
+                a=p.area()**(1/2)
+                x1 = i-a/2
+                y1 = random.uniform(j-a/2,j+a/2)
+                x3 = i+a/2
+                y3 = random.uniform(j-a/2,j+a/2)
+
+                x2 = random.uniform(i-a/2,i+a/2)
+                y2 = j-a/2
+                x4 = random.uniform(i-a/2,i+a/2)
+                y4 = j+a/2
                 x.extend([x1, x2, x3, x4, x1, None])
                 y.extend([y1, y2, y3, y4, y1, None])
     fig = go.Figure(data=go.Scattergl(x=x, y=y, mode='lines',
                                       line=dict(color='blue', width=1)))
+#碎片内部灰度值为均匀分布U(m,n)的结果，碎片是实心的
+#v = 150*sigma/dist()
+#m=原灰度-v
+#n=原灰度+v
+
+
     #fig.show()
     fig.update_layout(
         autosize=False,
