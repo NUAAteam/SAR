92e8d63b949c566ac6d63d42a8616b0edb1b47b8
Ebotian <yiboxiaotian@nuaa.edu.cn>
2024-04-20 16:07:24 +0800

    update app with bugs

---
+++ week3/anotherapp.py
@@ -0,0 +1,28 @@
+# app.py
+import cv2
+import numpy as np
+import plotly.graph_objs as go
+import io
+import streamlit as st
+import draw  # import my module
+
+def main():
+  st.title('My App')
+
+  # Create sliders
+  dm = st.slider('dm', min_value=0, max_value=2, value=1)
+  dn = st.slider('dn', min_value=0, max_value=2, value=1)
+  ic = st.slider('ic', min_value=0, max_value=100, value=50)
+  jc = st.slider('jc', min_value=0, max_value=100, value=50)
+  k = st.slider('k', min_value=0, max_value=3000, value=500)
+  sigma = st.slider('sigma', min_value=0, max_value=3, value=1)
+
+  # Generate the figure
+  picture=cv2.imread('week3/assets/nuaa_sar.jpg',cv2.IMREAD_GRAYSCALE)
+  fig = draw.draw_many_splinter(dm, dn, ic, jc, k, sigma,picture)
+
+  # Display the figure
+  st.plotly_chart(fig)
+
+if __name__ == '__main__':
+  main()
