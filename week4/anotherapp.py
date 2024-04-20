0fb45beff2db53124427097cefb6f52bee8c219f
Ebotian <yiboxiaotian@nuaa.edu.cn>
2024-04-20 16:57:34 +0800

    Read the image from the browser

--- week4/anotherapp.py
+++ week4/anotherapp.py
@@ -1,6 +1,7 @@
 import cv2
 import streamlit as st
 import draw  # import my module
+import numpy as np
 
 def main():
   st.title('高分辨率 SAR 图像打击效果评估')
@@ -14,7 +15,16 @@ def main():
   sigma = st.slider('sigma', min_value=0.0, max_value=3.0, value=1.0,step=0.1)
 
   # Generate the figure
-  picture=cv2.imread('week4/assets/nuaa_sar.jpg',cv2.IMREAD_GRAYSCALE)
+  #todo 读取图片 from browser
+  #picture=cv2.imread('week4/assets/nuaa_sar.jpg',cv2.IMREAD_GRAYSCALE)
+  # Read the image from the browser
+  uploaded_file = st.file_uploader("Choose an image...", type="jpg")
+  if uploaded_file is not None:
+    file_bytes = np.asarray(bytearray(uploaded_file.read()), dtype=np.uint8)
+    picture = cv2.imdecode(file_bytes, cv2.IMREAD_GRAYSCALE)
+  else:
+    picture = cv2.imread('week4/assets/nuaa_sar.jpg', cv2.IMREAD_GRAYSCALE)
+
   fig = draw.draw_many_splinter(dm, dn, ic, jc, k, sigma,picture)
 
   # Display the figure
