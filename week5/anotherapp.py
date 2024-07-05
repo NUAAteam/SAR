89df84563a3d323787882c3f5d04f00d6033b9b9
Ebotian_win <yiboxiaotian@nuaa.edu.cn>
2024-07-05 19:16:00 +0800

    整理文件到第五周期week5

--- week5/anotherapp.py
+++ week5/anotherapp.py
@@ -4,13 +4,27 @@ import draw  # import my module
 import numpy as np
 import matplotlib.pyplot as plt
 import plotly.graph_objects as go
+def plot_difference(original_picture, picture):
+    # Compute the absolute difference
+    difference = np.abs(original_picture.astype(int) - picture.astype(int))
 
+    # Convert the difference to uint8
+    difference = difference.astype(np.uint8)
+
+    # Create the x, y, and z coordinate arrays
+    y, x = np.mgrid[:difference.shape[0], :difference.shape[1]]
+    z = difference
+
+    # Create a 3D surface plot
+    fig = go.Figure(data=[go.Surface(z=z, x=x, y=y)])
+
+    return fig
 def main():
   st.title('高分辨率 SAR 图像打击效果评估')
 
   # Create sliders
-  dm = st.slider('横向打击精度', min_value=0.0, max_value=2.0, value=1.0, step=0.1)
-  dn = st.slider('纵向打击精度', min_value=0.0, max_value=2.0, value=1.0,step=0.1)
+  dm = st.slider('横向打击分辨度', min_value=0.0, max_value=2.0, value=1.0, step=0.1)
+  dn = st.slider('纵向打击分辨度', min_value=0.0, max_value=2.0, value=1.0,step=0.1)
   col1, col2 = st.columns(2)
   # Create sliders in each column
   ic = col1.number_input('原爆点X轴坐标', min_value=0, max_value=10000, value=150)
@@ -20,41 +34,30 @@ def main():
 
   # Generate the figure
   #todo 读取图片 from browser
-  #picture=cv2.imread('week4/assets/nuaa_sar.jpg',cv2.IMREAD_GRAYSCALE)
+
   # Read the image from the browser
   uploaded_file = st.file_uploader("Choose an image...", type="jpg")
   if uploaded_file is not None:
     file_bytes = np.asarray(bytearray(uploaded_file.read()), dtype=np.uint8)
     picture = cv2.imdecode(file_bytes, cv2.IMREAD_GRAYSCALE)
-    #picture = cv2.imdecode(file_bytes, cv2.IMREAD_UNCHANGED)
   else:
     picture = cv2.imread('week4/assets/nuaa_sar.jpg', cv2.IMREAD_GRAYSCALE)
 
-  #fig = draw.draw_many_splinter(dm, dn, ic, jc, k, sigma,picture)
+
   st.image(picture, caption='原始图像', use_column_width=True)
   original_picture = picture.copy()
   picture=draw.process_picture(dm, dn, ic, jc, k, sigma, picture)
   st.image(picture, caption='处理后图像', use_column_width=True)
 
-# Compute the absolute difference
-  difference = np.abs(original_picture.astype(int) - picture.astype(int))
+  # Create a 3D surface plot
+  fig = plot_difference(original_picture, picture)
 
-# Convert the difference to uint8
-  difference = difference.astype(np.uint8)
-
-# Create the x, y, and z coordinate arrays
-  y, x = np.mgrid[:difference.shape[0], :difference.shape[1]]
-  z = difference
-
-# Create a 3D surface plot
-  fig = go.Figure(data=[go.Surface(z=z, x=x, y=y)])
-
-# Update layout options
+  # Update layout options
   fig.update_layout(title='灰度差异的3D图像表示', autosize=False,
                   width=500, height=500,
                   margin=dict(l=65, r=50, b=65, t=90))
 
-# Display the figure
+  # Display the figure
   st.plotly_chart(fig)
 
   # Create a new figure and axes
