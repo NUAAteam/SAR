2385d1544eb9d55654ffa0590313075516fdf2cd
Ebotian_win <yiboxiaotian@nuaa.edu.cn>
2024-07-05 18:50:24 +0800

    homework

---
+++ homework/kkk.md
@@ -0,0 +1,9 @@
+| E \ EC | NB  | NM  | NS  | Z0  | PS  | PM  | PB  |
+|--------|-----|-----|-----|-----|-----|-----|-----|
+| NB     | PB  | PB  | PB  | PB  | PM  | PS  | Z0  |
+| NM     | PB  | PB  | PM  | PM  | PS  | Z0  | Z0  |
+| NS     | PB  | PM  | PM  | PS  | Z0  | Z0  | NS  |
+| Z0     | PB  | PM  | PS  | Z0  | NS  | NM  | NB  |
+| PS     | PM  | PS  | Z0  | NS  | NM  | NM  | NB  |
+| PM     | PS  | Z0  | NS  | NM  | NM  | NB  | NB  |
+| PB     | Z0  | NS  | NM  | NB  | NB  | NB  | NB  |
--- homework/matlab.m
+++ homework/matlab.m
@@ -1,41 +1,48 @@
-%dr/dt=V_m*cos(eta_m)-V_d*cos(eta)
-%r*dq/dt=V_d*sin(eta)-V_m*sin(eta_m)
-%q=sigma+eta
-%q=sigma_m+eta_m
-%epsilon=0
 
-% Define the initial conditions
-r0 = 300;  % Define r0
-q0 = pi/2;  % Define q0
-eta0 = ...;  % Define eta0
-sigma0 = q0-eta0;  % Define sigma0
-sigma_m0 = ...;  % Define sigma_m0
-% Define the initial conditions and the time span
-y0 = [r0; q0; sigma0; sigma_m0; eta0];  % Initial conditions
-tspan = [t_start t_end];  % Time span
+r0 = 300;
+q0 = pi/3;
+sigma0 = pi/6;
+eta0 = q0-sigma0;
+sigma_m0 = pi/4;
+eta_m0 = q0 - sigma_m0;
 
-% Define the known parameters
-V_m = 20;  % Define V_m
-V_d = 20;  % Define V_d
-eta_m = pi/3;  % Define eta_m
-eta = 0;  % Define eta
+y0 = [r0; q0; sigma0; eta0; sigma_m0; eta_m0];
+yp0 = [0; 0; 0; 0; 0; 0];  % initial derivatives
 
-% Solve the system of differential equations
-[t, y] = ode45(@(t, y) system(t, y, V_m, V_d, eta_m, eta), tspan, y0);
-function dydt = system(t, y, V_m, V_d, eta_m, eta)
-  % Define the variables
-  r = y(1);
-  q = y(2);
-  sigma = y(3);
-  sigma_m = y(4);
-  eta = y(5);
+tspan = [0 10];
+[t, y] = ode15i(@dae, tspan, y0, yp0);
 
-  % Define the system of differential equations
-  dydt = zeros(5, 1);
-  dydt(1) = V_m*cos(eta_m) - V_d*cos(eta);  % dr/dt
-  dydt(2) = (V_d*sin(eta) - V_m*sin(eta_m))/r;  % dq/dt
-  dydt(3) = q - eta;  % sigma
-  dydt(4) = q - eta_m;  % sigma_m
-  %追踪法
-  dydt(5) = 0;  % eta
+% Plot r curve
+figure;
+plot(t, y(:, 1));
+title('r over time');
+xlabel('Time');
+ylabel('r');
+grid on;
+
+function res = dae(t, y, yp)
+    V_m = 30;
+    V_d = 50;
+    sigma_m = pi/4;
+    q_0 = pi/3;
+    eta_m = q_0 - sigma_m;
+
+    r = y(1);
+    drdt = yp(1);
+    q = y(2);
+    dqdt = yp(2);
+    sigma = y(3);
+    eta = y(4);
+    sigma_m = y(5);
+    eta_m = y(6);
+
+    res = zeros(6, 1);
+
+    res(1) = drdt - V_m*cos(eta_m) + V_d*cos(eta);
+    res(2) = dqdt - V_d*sin(eta) + V_m*sin(eta_m);
+    res(3) = q - sigma - eta;
+    res(4) = q - sigma_m - eta_m;
+    res(5) = q - q_0;
+    res(6) = 0;  % epsilon = 0
 end
+
