6318a37376b5b8330b1f074ee3ad788ad14d4ec9
Ebotian <yiboxiaotian@nuaa.edu.cn>
2024-07-15 13:30:55 +0000

    新增噪声和界面

--- week5/main.py
+++ week5/main.py
@@ -7,6 +7,7 @@ def main():
     "主界面",
     "图像打击效果仿真",
     "高分辨率SAR图像仿真"
+    "打击目标"
     ])
 
   if page == "主界面":
@@ -27,6 +28,21 @@ def main():
       # 第二个子页面内容
       st.title("高分辨率SAR图像仿真")
       sar()
+  elif page == "打击目标":
+      # 新增的打击目标功能区
+      st.title("打击目标")
+      uploaded_files = st.file_uploader("上传图片", accept_multiple_files=True, type=['jpg', 'jpeg', 'png'], help="一次性上传多张图片")
+
+      if uploaded_files:
+          for uploaded_file in uploaded_files:
+              st.image(uploaded_file, caption=uploaded_file.name, width=100)
+
+          selected_action = st.selectbox("选择操作", ["请选择操作", "高分辨率SAR图像仿真", "图像打击效果仿真"])
+
+          if selected_action == "高分辨率SAR图像仿真":
+              sar()
+          elif selected_action == "图像打击效果仿真":
+              simulate()
 
 if __name__ == "__main__":
     main()
